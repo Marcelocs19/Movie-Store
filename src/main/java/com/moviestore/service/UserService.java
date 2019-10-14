23020f199f729120e058dbfead915000c8189cd3
Marcelo <m_campos_19@hotmail.com>
2019-10-14 12:22:32 -0300

    :white_check_mark: Test that successfully creates a user

--- src/main/java/com/moviestore/service/UserService.java
+++ src/main/java/com/moviestore/service/UserService.java
@@ -8,17 +8,17 @@ import com.moviestore.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	private final UserRepository userRepository;
 
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
 	}
-	
+
 	public User createNewUser(UserForm userForm) {
-			User user = new User(userForm.getName(),userForm.getEmail(),userForm.getPassword());
-			userRepository.saveAndFlush(user);
-			return user;
+		User user = new User(userForm.getName(), userForm.getEmail(), userForm.getPassword());
+		userRepository.saveAndFlush(user);
+		return user;
 	}
 }
--- src/test/java/com/moviestore/controller/UserControllerTest.java
+++ src/test/java/com/moviestore/controller/UserControllerTest.java
@@ -1,5 +1,46 @@
 package com.moviestore.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moviestore.MovieStoreApplication;
+import com.moviestore.controller.form.UserForm;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MovieStoreApplication.class)
+@WithMockUser(roles = "admin")
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
+	private static final String PATH_CREATE_NEW_USER = "/users";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	
+	@Test
+	public void testCreateUserSucess() throws Exception {
+		UserForm newUser = new UserForm();	
+		newUser.setEmail("allan@gmail.com");
+		newUser.setName("Allan");
+		newUser.setPassword("abcd");
+		ObjectMapper mapper = new ObjectMapper();
+		String newUserAsJSON = mapper.writeValueAsString(newUser);
+		mockMvc.perform(post(PATH_CREATE_NEW_USER)
+				.content(newUserAsJSON).accept(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isCreated());
+	}
+	
 }
